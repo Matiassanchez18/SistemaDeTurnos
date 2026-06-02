cb39d7483fb9480e93effc6f37947cab00cb1bde
Matias Sanchez <matiassanchez13422@gmail.com>
2026-06-01 22:28:33 -0300

    Se agrego la logica para verificar si el usuario y la contrseña ingresada son correctas

--- js/Index.js
+++ js/Index.js
@@ -1,1 +1,19 @@
-console.log("Hola")
+const btn = document.getElementById("btnEnviar");
+
+const verificarUsuario = () => {
+  const inputUsuario = document.getElementById("usuario").value;
+  const inputContraseña = document.getElementById("contraseña").value;
+
+  const admin = "admin1234";
+  const contraseña = "12345";
+
+ if (
+  admin.toLocaleLowerCase() === inputUsuario.toLocaleLowerCase() &&
+  contraseña === inputContraseña
+) {
+  console.log("si");
+} else {
+  console.log("no");
+}
+}
+btn.addEventListener("click", verificarUsuario);
