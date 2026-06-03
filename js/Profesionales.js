cd6e5cacd26e12795b585ba17685d38ca50c5d25
Matias Sanchez <matiassanchez13422@gmail.com>
2026-06-03 18:27:17 -0300

    Se agrego la logica para actualizar los profesionales

--- js/Profesionales.js
+++ js/Profesionales.js
@@ -1,15 +1,28 @@
+const actualizarProfesional = async () => {
+  const inputNombre = document.getElementById("Nombre").value;
+  const inputMatricula = document.getElementById("Matricula").value;
+  const inputEspecialidad = document.getElementById("Especialidad").value;
+  const inputDias = document.getElementById("diasatencion").value;
 
-const actualizarProfesional=async()=>{
-const inputNombre=document.getElementById("Nombre").value
-const inputMatricula=document.getElementById("Matricula").value
-const inputEspecialidad=document.getElementById("Especialidad").value
-const inputDias =document.getElementById("diasatencion").value
+  try {
+    const consulta = await axios.get("http://localhost:3000/Profesionales");
+    const datos = await consulta.data;
+    const tabla = document.getElementById("tabla");
 
-try{
-const consulta = await axion.get()
+    datos.forEach((profesionales) => {
+      const tr = document.createElement("tr");
+      tr.innerHTML = `
+      <th>${profesionales.id}</th>
+      <th>${profesionales.nombreCompleto}</th>
+      <th>${profesionales.Matricula}</th>
+      <th>${profesionales.Especialidad}</th>
+      <th>${profesionales.Dias}</th>
+`;
+      tabla.appendChild(tr);
+    });
+  } catch (error) {
+    console.log("ocurrio un error " + error.message);
+  }
+};
 
-}catch(error){
-    console.log("ocurrio un error "+ error.message)
-}
-
-}
+actualizarProfesional()
