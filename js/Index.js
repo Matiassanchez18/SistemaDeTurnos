9f67e7565b7f518a9c5453df51148196a2f08ac4
Matias Sanchez <matiassanchez13422@gmail.com>
2026-06-18 19:10:55 -0300

    Se corrigio los estados

--- js/Index.js
+++ js/Index.js
@@ -20,7 +20,7 @@ const verificarUsuario = () => {
                 <a class="nav-link" href="./pages/Pacientes.html">Pacientes</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="./pages/admin.html">admin</a>
+                <a class="nav-link" href="./pages/admin.html">Turnos</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="./pages/Profesionales.html">Profesionales</a>
